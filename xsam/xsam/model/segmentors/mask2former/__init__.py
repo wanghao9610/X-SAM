f090035be5ce4ee7bdeffef9665e7715c73b1d15
wanghao9610 <wanghao9610@outlook.com>
2025-08-13 20:15:52 +0800

    Remove commented-out imports in mask2former module to clean up the code and improve readability.

--- xsam/xsam/model/segmentors/mask2former/__init__.py
+++ xsam/xsam/model/segmentors/mask2former/__init__.py
@@ -10,18 +10,6 @@ from .modeling_mask2former import (
     Mask2FormerTransformerModule,
 )
 
-# from .configuration_mask2former_v1 import Mask2FormerConfig
-# from .modeling_mask2former_v1 import (
-#     Mask2FormerLoss,
-#     Mask2FormerMaskedAttentionDecoderLayer,
-#     Mask2FormerModel,
-#     Mask2FormerPixelDecoder,
-#     Mask2FormerPixelDecoderEncoderMultiscaleDeformableAttention,
-#     Mask2FormerPixelDecoderEncoderOnly,
-#     Mask2FormerPixelLevelModule,
-#     Mask2FormerTransformerModule,
-# )
-
 __all__ = [
     "Mask2FormerConfig",
     "Mask2FormerModel",
